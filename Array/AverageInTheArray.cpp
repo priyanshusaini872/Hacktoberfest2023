1ba1cbdf5ae96ab3bd070abc63ebfa6b429411c5
priyanshusaini872 <142074515+priyanshusaini872@users.noreply.github.com>
2023-10-15 20:08:39 +0530

    Priyanshu average of array elements.cpp

--- Array/AverageInTheArray.cpp
+++ Array/AverageInTheArray.cpp
@@ -1,24 +1,27 @@
 // C++ program to calculate average of array elements
 #include <iostream>
-using namespace std;
 
-// Function that return average of an array.
-double average(int a[], int n)
-{
-	// Find sum of array element
-	int sum = 0;
-	for (int i=0; i<n; i++)
-	sum += a[i];
+int main() {
+    int n;
+    std::cout << "Enter the number of elements in the array: ";
+    std::cin >> n;
 
-	return (double)sum/n;
-}
+    if (n <= 0) {
+        std::cout << "Invalid input. Number of elements should be greater than 0." << std::endl;
+        return 1;
+    }
 
-// Driver code
-int main()
-{
-	int arr[] = {10, 2, 3, 4, 5, 6, 7, 8, 9};
-	int n = sizeof(arr)/sizeof(arr[0]);
+    double sum = 0;
+    int arr[n];
 
-	cout << average(arr, n) << endl;
-	return 0;
+    std::cout << "Enter the elements of the array, separated by spaces:" << std::endl;
+    for (int i = 0; i < n; i++) {
+        std::cin >> arr[i];
+        sum += arr[i];
+    }
+
+    double average = sum / n;
+    std::cout << "The average of the elements in the array is: " << average << std::endl;
+
+    return 0;
 }
